c1deda4a407ef075f5f277696a699c2a1583d2b0
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-05 13:12:01 -0800

    Explained the game to user

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,7 +1,10 @@
 # Greet the user and ask their name
 print("Hello and welcome to the Number Guessing Game!")
-input("What's your name? ")
-# Explain rules
+name = input("What's your name? ").strip().title()
+
+# Explain game
+print(f"Hello, {name}")
+print("This game will choose a random number from 1-100. You'll be given 6 guesses. Don't worry I'll give hints ")
 # Choose a random number
 # Make a loop for the amount of guesses
 # Ask to play again
