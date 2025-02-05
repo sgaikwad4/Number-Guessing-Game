c8481c1a3d830905d03a2c88b0d561e2041ca94f
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-05 13:05:26 -0800

    Greeted user and asked for name

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,4 +1,6 @@
 # Greet the user and ask their name
+print("Hello and welcome to the Number Guessing Game!")
+input("What's your name? ")
 # Explain rules
 # Choose a random number
 # Make a loop for the amount of guesses
